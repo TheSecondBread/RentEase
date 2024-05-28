1a0d7d6c0674951452187393a940696c77e58665
TheSecondBread <136884806+TheSecondBread@users.noreply.github.com>
2024-05-28 11:21:48 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import Intro from './components/Intro.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
-import Profile from './components/Profile.jsx'
+
 import Createprop from './components/Createprop.jsx'
 import Myprop from './components/Myprop.jsx'
 import Allprops from './components/Allprops.jsx'
@@ -17,7 +17,6 @@ const route =createBrowserRouter([
   {path:"/",element:<Intro></Intro>},
   {path:"/home",element:<Home></Home>},
   {path:"/about",element:<About></About>},
-  {path:"/profile",element:<Profile></Profile>},
   {path:"/list",element:<Createprop></Createprop>},
   {path:"/myprop",element:<Myprop></Myprop>},
   {path:"/browse",element:<Allprops></Allprops>}
